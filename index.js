2315ed46685c5af32336bceb17bcb397b9a52c73
replakcan <alper.makcan@gmail.com>
2025-03-10 01:10:27 +0300

    Creates localStorage darkMode setter func
    and changes body bgClr depends on func rv

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 import btnGroup from './components/layout/btnGroup.js'
 import sketchGridContainer from './components/layout/sketchGridContainer.js'
+import isUserSystemDark from './lib/set-theme-local-storage.js'
+
+isUserSystemDark()
+
+const theme = JSON.parse(localStorage.getItem('darkTheme'))
 
 const body = document.querySelector('body')
+
+body.style.backgroundColor = theme ? '#3c3d37' : 'rgb(202, 238, 220)'
 
 body.appendChild(btnGroup)
 body.appendChild(sketchGridContainer)

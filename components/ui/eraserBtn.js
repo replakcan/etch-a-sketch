d2f0589a4e1ea6ef249f449d50d042dd789cac49
replakcan <alper.makcan@gmail.com>
2025-03-10 01:11:17 +0300

    Eraser btn makes grid bg transparent now on

--- components/ui/eraserBtn.js
+++ components/ui/eraserBtn.js
@@ -4,7 +4,9 @@ eraserBtn.textContent = 'clear'.toUpperCase()
 eraserBtn.addEventListener('click', () => {
   const sketchDivs = [...document.querySelectorAll('.sketch-div')]
 
-  sketchDivs.map((div) => (div.style.backgroundColor = 'white'))
+  sketchDivs.map((div) => {
+    div.style.backgroundColor = 'transparent'
+  })
 })
 
 export default eraserBtn

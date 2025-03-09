d75e2652e00ef939170d8d84575eb932049414a6
replakcan <alper.makcan@gmail.com>
2025-03-09 15:43:51 +0300

    Adds function imports at container component

--- components/ui/gridSetterBtn.js
+++ components/ui/gridSetterBtn.js
@@ -1,6 +1,8 @@
 import askUserGridAmount from "../../lib/ask-user-grid-amount.js"
 import emptyGridSystem from "../../lib/empty-grid-system.js"
 import createGridSystem from "../../lib/grid-system-creator.js"
+import { randomRGBGenerator } from "../../lib/random-color-generator.js"
+import sketchGridContainer from "../layout/sketchGridContainer.js"
 
 const gridSetterBtn = document.createElement('button')
 gridSetterBtn.textContent = 'grids per row'.toUpperCase()
@@ -11,8 +13,8 @@ gridSetterBtn.addEventListener('click', () => {
     divCountOneSide = askUserGridAmount()
   } while (divCountOneSide > 100 || divCountOneSide < 10)
 
-  emptyGridSystem(sketchDivContainer)
-  createGridSystem(window.innerWidth / divCountOneSide, sketchDivContainer, randomRGBGenerator)
+  emptyGridSystem(sketchGridContainer)
+  createGridSystem(window.innerWidth / divCountOneSide, sketchGridContainer, randomRGBGenerator)
 })
 
 export default gridSetterBtn

85e1af6fbee4ad07769ab29bc41dd49130a21aac
replakcan <alper.makcan@gmail.com>
2025-03-10 01:13:13 +0300

    Toggles component themes between dark/light preferences

--- components/layout/btnGroup.js
+++ components/layout/btnGroup.js
@@ -1,10 +1,16 @@
 import eraserBtn from '../ui/eraserBtn.js'
 import gridSetterBtn from '../ui/gridSetterBtn.js'
+import toggleThemeBtn from '../ui/toggleThemeBtn.js'
+
+let theme = JSON.parse(localStorage.getItem('darkTheme'))
 
 const btnGroup = document.createElement('div')
 btnGroup.classList.add('btn-group')
 
+btnGroup.style.backgroundColor = theme ? '#1e201e' : 'rgb(67, 55, 136)'
+
 btnGroup.appendChild(gridSetterBtn)
 btnGroup.appendChild(eraserBtn)
+btnGroup.appendChild(toggleThemeBtn)
 
 export default btnGroup
--- components/layout/sketchGridContainer.js
+++ components/layout/sketchGridContainer.js
@@ -1,4 +1,8 @@
 const sketchGridContainer = document.createElement('div')
 sketchGridContainer.classList.add('container')
 
+let theme = JSON.parse(localStorage.getItem('darkTheme'))
+
+sketchGridContainer.style.backgroundColor = theme ? '#1e201e' : 'rgb(67, 55, 136)'
+
 export default sketchGridContainer
